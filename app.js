3ed02ef469b22f5db1117a31eb63d6d9b496acb1
SL0TR <mohaiminislam94@gmail.com>
2018-07-14 18:23:55 +0600

    serve a json with nodejs

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var fs = require('fs');
 
 var server = http.createServer((req, res) => {
   console.log('Request was made from: ' + req.url);
-  res.writeHead(200, {'Content-type' : 'text/html'});
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  myReadStream.pipe(res)
+  res.writeHead(200, {'Content-type' : 'application/json'});
+  var myObj = {
+    name: 'Mohaimin',
+    job: 'Full Stack Develioer ;)',
+    age: 24
+  };
+  res.end(JSON.stringify(myObj));
 })
 
 server.listen(3000, '127.0.0.1');
-console.log('You are now listening to port 3000');
+console.log('You are now listening to port 3000'); 
