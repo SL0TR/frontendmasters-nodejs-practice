32aa4814798722f0e49a916cd8c69266306c6f89
SL0TR <mohaiminislam94@gmail.com>
2018-07-14 12:54:07 +0600

    use events and util mode with eventEmitter constructor

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-var stuff = require('./stuff');
+// var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.counter(['shaun', 'bladu', 'kapi', 'shudha']));
-console.log(stuff.adder(5,7));
-console.log(stuff.pi);
+// Stuff Module
+// console.log(stuff.counter(['shaun', 'bladu', 'kapi', 'shudha']));
+// console.log(stuff.adder(5,7));
+// console.log(stuff.pi);
+
+// var myEmitter = new events.EventEmitter();
+// myEmitter.on('someEvent', (msg) => {
+//   console.log(msg);
+// })
+
+// myEmitter.emit('someEvent', 'The event has been emitted');
+
+var Person = function(name) {
+  this.name = name;
+}
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('James');
+var lanu = new Person('Lanu');
+var kalu = new Person('Kalu');
+
+var people = [james, lanu, kalu];
+
+people.forEach((person) => {
+  person.on('speak', (msg) => {
+    console.log(person.name + ' said: ' + msg);
+  });
+})
+
+james.emit('speak', 'hey dudes');
+kalu.emit('speak', 'I want boobs');
