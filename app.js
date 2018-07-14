6ba1df38bf9df0afa7e321ff3e01b57a291a9bdb
SL0TR <mohaiminislam94@gmail.com>
2018-07-14 18:01:55 +0600

    use writable streams with chunks

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writedme.txt');
 
 myReadStream.on('data', (chunk) => {
   console.log('new chunk recieved:');
-  console.log(chunk);
-})
+  myWriteStream.write(chunk);
+});
