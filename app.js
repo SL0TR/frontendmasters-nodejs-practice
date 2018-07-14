99a2796b898070ddbb4c5ee4cc9fd16f8fff81d2
SL0TR <mohaiminislam94@gmail.com>
2018-07-15 01:08:37 +0600

    setup fs and add public path for static file get request access

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index');
