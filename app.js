1735298ef66ac14626fbc120811601f816dd52ba
SL0TR <mohaiminislam94@gmail.com>
2018-07-17 18:23:14 +0600

    add post request handling through body parser

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  // console.log(req.query);
+  res.render('index', {qs: req.query});
+});
+
+app.post('/', urlencodedParser, (req, res) => {
+  console.log(req.body);
+  res.render('index', {qs: req.query});
 });
 
 app.get('/profile/:name', (req, res) => {
