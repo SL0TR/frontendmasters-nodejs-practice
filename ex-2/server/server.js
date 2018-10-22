75a735ef8066a642b8686dd4b31c3d0738ce433d
SL0TR <mohaiminislam94@gmail.com>
2018-10-22 13:46:06 +0600

    finish ex-2 CRUD REST API

--- ex-2/server/server.js
+++ ex-2/server/server.js
@@ -21,7 +21,25 @@ app.use(bodyParser.json());
 var lions = [];
 var id = 0;
 
-// TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res){
+  res.json(lions)
+})
+
+app.get('/lions/:id', function(req, res){
+  var lion = _.find(lions, {id: req.params.id})
+  res.json(lion || {})
+})
+
+app.post('/lions', function(req, res){
+  var lion = req.body
+  id++
+  lion.id = id + ''
+  lions.push(lion)
+  res.json(lion)
+  console.log(lions)
+})
+
+// app.put('lions/:')
 
 app.listen(3000);
 console.log('on port 3000');
