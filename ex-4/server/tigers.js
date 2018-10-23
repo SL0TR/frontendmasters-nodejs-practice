4c3748ece05413ef930c107b61938bf3eaaa70d8
SL0TR <mohaiminislam94@gmail.com>
2018-10-24 01:54:58 +0600

    refactor router file

--- ex-4/server/tigers.js
+++ ex-4/server/tigers.js
@@ -22,37 +22,46 @@ tigerRouter.param('id', function(req, res, next, id) {
   }
 });
 
-tigerRouter.get('/', function(req, res){
-  res.json(tigers);
-});
+tigerRouter.route('/')
+  .get(function(req, res){
+    res.json(tigers);
+  })
+  .post(updateId, function(req, res) {
+    var tiger = req.body;
+    tigers.push(tiger);
+    res.json(tiger);
+  });
 
-tigerRouter.get('/:id', function(req, res){
-  var tiger = req.todo;
-  res.json(tiger || {});
-});
+tigerRouter.route('/:id')
+  .get(function(req, res){
+    var tiger = req.todo;
+    res.json(tiger || {});
+  })
+  .put(function(req, res) {
+    var update = req.body;
+    if (update.id) {
+      delete update.id
+    }
+  
+    var tiger = _.findIndex(tigers, {id: req.params.id});
+    if (!tigers[tiger]) {
+      res.send();
+    } else {
+      var updatedtiger = _.assign(tigers[tiger], update);
+      res.json(updatedtiger);
+    }
+  })
+  .delete(function(req, res) {
 
-tigerRouter.post('/', updateId, function(req, res) {
-  var tiger = req.body;
-
-  tigers.push(tiger);
-
-  res.json(tiger);
-});
-
-
-tigerRouter.put('/:id', function(req, res) {
-  var update = req.body;
-  if (update.id) {
-    delete update.id
-  }
-
-  var tiger = _.findIndex(tigers, {id: req.params.id});
-  if (!tigers[tiger]) {
-    res.send();
-  } else {
-    var updatedtiger = _.assign(tigers[tiger], update);
-    res.json(updatedtiger);
-  }
-});
+    var lion = _.findIndex(lions, {id: req.params.id})
+    if (!lions[lion]) {
+      res.send()
+    } else {
+      var deletedLion = lions[lion]
+      lions.splice(lion, 1)
+      res.json(deletedLion)
+    }
+  
+  })
 
 module.exports = tigerRouter;
